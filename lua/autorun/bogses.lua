cabcd79723ea3d8aed1646d14b92d54a8933e36a
mamietti <mauri.miettinen13@gmail.com>
2017-09-04 22:34:28 +0300

    Removed soundscript definitions

--- lua/autorun/bogses.lua
+++ lua/autorun/bogses.lua
@@ -34,6 +34,7 @@ if CLIENT then
     language.Add( "GaussEnergy_ammo", "Depleted Uranium-235" )
     language.Add( "SniperRound_ammo", "Sniper Ammo" )
     language.Add( "Manhack_ammo", "Manhacks" )
+    language.Add( "FlareRound_ammo", "Flares" )
 end
 
 game.AddAmmoType( {
@@ -46,83 +47,3 @@ game.AddAmmoType( {
 	minsplash = 10,
 	maxsplash = 5
 } )
-sound.Add( {
-    name = "Weapon_HMG1.Single",
-    channel = CHAN_WEAPON,
-    volume = 1.0,
-    level = SNDLVL_GUNFIRE,
-    pitch = 100,
-    sound = {"weapons/hmg1/hmg1_7.wav","weapons/hmg1/hmg1_7.wav","weapons/hmg1/hmg1_7.wav"}
-} )
-sound.Add( {
-    name = "Weapon_Chargebow.ChargeStart",
-    channel = CHAN_WEAPON,
-    volume = 1.0,
-    level = SNDLVL_IDLE,
-    pitch = 80,
-    sound = {"weapons/drink_up.wav"}
-} )
-sound.Add( {
-    name = "Weapon_Sniper.Single",
-    channel = CHAN_WEAPON,
-    volume = 1.0,
-    level = SNDLVL_GUNFIRE,
-    pitch = 100,
-    sound = {"weapons/sniper/sniper_fire1.wav"}
-} )
-sound.Add( {
-    name = "Weapon_Sniper.BoltBack",
-    channel = CHAN_ITEM,
-    volume = 1.0,
-    level = SNDLVL_IDLE,
-    pitch = 100,
-    sound = {"weapons/sniper/sniper_boltback.wav"}
-} )
-sound.Add( {
-    name = "Weapon_Sniper.BoltForward",
-    channel = CHAN_ITEM,
-    volume = 1.0,
-    level = SNDLVL_IDLE,
-    pitch = 100,
-    sound = {"weapons/sniper/sniper_boltforward.wav"}
-} )
-sound.Add( {
-    name = "Weapon_Sniper.ClipIn",
-    channel = CHAN_ITEM,
-    volume = 1.0,
-    level = SNDLVL_IDLE,
-    pitch = 100,
-    sound = {"weapons/sniper/sniper_clipin.wav"}
-} )
-sound.Add( {
-    name = "Weapon_Sniper.ClipOut",
-    channel = CHAN_ITEM,
-    volume = 1.0,
-    level = SNDLVL_IDLE,
-    pitch = 100,
-    sound = {"weapons/sniper/sniper_clipout.wav"}
-} )
-sound.Add( {
-    name = "Weapon_9mmAR.Reload",
-    channel = CHAN_ITEM,
-    volume = 1.0,
-    level = SNDLVL_IDLE,
-    pitch = 100,
-    sound = {"weapons/9mmar/9mmar_reload.wav"}
-} )
-sound.Add( {
-    name = "Weapon_TripleSMG.Single",
-    channel = CHAN_WEAPON,
-    volume = 1.0,
-    level = SNDLVL_GUNFIRE,
-    pitch = 120,
-    sound = "weapons/smg1/smg1_triple_fire1.wav"
-} )
-local NPC = {
-	Name = "Robo Grunt",
-	Class = "npc_robo_grunt",
-	Category = "Robot",
-	KeyValues = { SquadName = "robot" }
-}
-list.Set( "NPC", NPC.Class, NPC )
-Add_NPC_Class( "CLASS_ROBOT" )
